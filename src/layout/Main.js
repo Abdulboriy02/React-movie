aecb2f7a03dfa286bf0a6e6bbbda168950ba60d3
Abdulboriy <abdulboriyyulchiyev02@gmail.com>
2022-04-09 08:10:19 -0400

    tamom

---
+++ src/layout/Main.js
@@ -0,0 +1,40 @@
+import React from "react";
+import Loader from "../components/Loader";
+import Movies from "../components/Movies";
+import Search from "../components/Search";
+
+export default class Main extends React.Component {
+  state = {
+    movies: [],
+    loading: true,
+  };
+  componentDidMount() {
+    fetch("http://www.omdbapi.com/?apikey=778d7da5&s=panda")
+      .then((response) => response.json())
+      .then((data) => this.setState({ movies: data.Search, loading: false }));
+  }
+
+  searchMovies = (str, type = "all") => {
+    this.setState({ loading: true });
+    fetch(
+      `http://www.omdbapi.com/?apikey=778d7da5&s=${str}${
+        type !== "all" ? `&type=${type}` : ""
+      }`
+    )
+      .then((response) => response.json())
+      .then((data) => this.setState({ movies: data.Search, loading: false }));
+  };
+
+  render() {
+    return (
+      <div className="container content">
+        <Search searchMovie={this.searchMovies} />
+        {this.state.loading ? (
+          <Loader />
+        ) : (
+          <Movies movies={this.state.movies} />
+        )}
+      </div>
+    );
+  }
+}
